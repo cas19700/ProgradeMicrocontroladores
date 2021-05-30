a7e45c7abf74ba4f0e6ff2f7a40b25164a056aac
cas19700 <68095306+cas19700@users.noreply.github.com>
2021-05-30 00:17:30 -0600

    Add files via upload

---
+++ Proyecto2.c
@@ -0,0 +1,168 @@
+/*
+ *Archivo:          Proyecto2.c
+ *Dispositivo:	    PIC16F887
+ *Autor:            Brayan Castillo
+ *Compilador:	    XC8
+ *Programa:         Animatronic Face
+ *Hardware:         Servos, motor DC, push button y LEDS
+ *Creado:           16 de mayo del 2021
+ *Ultima modificacion:	4 de junio del 2021
+*/
+//******************************************************************************
+//Importaciòn de librerias
+//****************************************************************************** 
+
+#include <xc.h>
+#include <stdint.h>
+
+//******************************************************************************
+//Palabras de configuracion
+//****************************************************************************** 
+    
+// CONFIG1
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO 
+                                    // oscillator: I/O function on 
+                                    // RA6/OSC2/CLKOUT pin, I/O function 
+                                    // on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF           // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON           // Power-up Timer Enable bit (PWRT enabled)
+#pragma config MCLRE = OFF          // RE3/MCLR pin function select bit 
+                                    // (RE3/MCLR pin function is MCLR)
+#pragma config CP = OFF             // Code Protection bit (Program memory code 
+                                    // protection is disabled)
+#pragma config CPD = OFF            // Data Code Protection bit (Data memory 
+                                    // code protection is disabled)
+#pragma config BOREN = OFF          // Brown Out Reset Selection bits 
+                                    // (BOR disabled)
+#pragma config IESO = OFF           // Internal External Switchover bit 
+                                    // (Internal/External Switchover mode 
+                                    // is disabled)
+#pragma config FCMEN = OFF          // Fail-Safe Clock Monitor Enabled bit 
+                                    // (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = ON             // Low Voltage Programming Enable bit 
+                                    // (RB3/PGM pin has PGM function, low 
+                                    // voltage programming enabled)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V       // Brown-out Reset Selection bit (Brown-out 
+                                    // Reset set to 4.0V)
+#pragma config WRT = OFF            // Flash Program Memory Self Write Enable 
+                                    // bits (Write protection off)
+
+//******************************************************************************
+//Directivas del compilador
+//******************************************************************************
+#define _tmr0_value 100 //Prescaler debe estar en 256 para 20 ms 
+#define _tmr1_value 65036 //Prescaler debe estar en 8 para 2 ms 
+#define _XTAL_FREQ 8000000  //Frecuencia
+
+//******************************************************************************
+//Variables
+//******************************************************************************
+uint8_t    num = 0;     //Variable para el numero
+uint8_t    band = 0;    //Variable banderas para el display
+uint8_t    cent = 0;    //Variable centenas
+uint8_t    dece = 0;    //Variable decenas
+uint8_t    uni = 0;     //Variable unidades
+int Display[10] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x67};
+                        //Array para la tabla
+
+//******************************************************************************
+//Prototipos de Funciones
+//******************************************************************************
+
+void setup(void);
+void rst_tmr0(void);
+void rst_tmr1(void);
+void __interrupt() isr(void)
+    { 
+    if(T0IF == 1){              //Interupcion del timer0
+        rst_tmr0();
+        PORTD = 0x00;           //Limpiar el PORTD
+        
+    }
+	if(TMR1IF == 1){              //Interrupcion del Puerto B
+        rst_tmr1();
+        PORTD = 0x01;
+        PIR1bits.TMR1IF = 0;
+      }
+    }
+//******************************************************************************
+//Ciclo Principal
+//******************************************************************************
+    
+    void main(void){
+        setup();                //Llamar las configutaciones
+        while(1)                //Siempre realizar el ciclo
+        {num = PORTA;           //La variable numero siempre igual al puerto A
+                     //Llamar la funcion para separar unidades
+        }
+    }
+//******************************************************************************
+//Configuracion
+//******************************************************************************
+
+    void setup(void){
+    //  Configuracion de puertos
+    ANSEL = 0x00;
+    ANSELH = 0x00;
+    //  Declaramos puertos como entradas o salidas
+    TRISA = 0x00;
+    TRISC = 0x00;
+    TRISD = 0xF8;
+    TRISB = 0x03;
+    //Limpiamos valores en los puertos
+    PORTA = 0x00;
+    PORTB = 0x00;
+    PORTC = 0x00;
+    PORTD = 0x00;
+    //Configuracion del oscilador
+    OSCCONbits.IRCF2 = 1;
+    OSCCONbits.IRCF1 = 1;
+    OSCCONbits.IRCF0 = 1;
+    OSCCONbits.SCS = 1;
+    //Configuracion del Timer0
+    OPTION_REGbits.T0CS = 0;
+    OPTION_REGbits.PSA = 0;
+    OPTION_REGbits.PS2 = 1;
+    OPTION_REGbits.PS1 = 1;
+    OPTION_REGbits.PS0 = 1;
+    rst_tmr0();
+    //Configuracion del Timer1
+    PIE1bits.TMR1IE = 1;
+    T1CONbits.TMR1CS = 0;
+    T1CONbits.T1CKPS1 = 1;
+    T1CONbits.T1CKPS0 = 1;
+    T1CONbits.T1OSCEN = 0;
+    T1CONbits.T1SYNC = 0;
+    T1CONbits.TMR1ON = 1;
+    rst_tmr1();
+    //Pullups
+    OPTION_REGbits.nRBPU = 0;
+    WPUBbits.WPUB0 = 1;
+    WPUBbits.WPUB1 = 1;
+    //Configuracion de interrupciones
+    INTCONbits.T0IE = 1;
+    INTCONbits.T0IF = 0;
+    INTCONbits.RBIE = 1;
+    INTCONbits.RBIF = 0;
+    //PUERTOB
+    IOCBbits.IOCB0 = 1;
+    IOCBbits.IOCB1 = 1;
+    INTCONbits.GIE = 1;
+    
+    }
+    void rst_tmr0(void){
+    TMR0 = _tmr0_value;
+    INTCONbits.T0IF = 0;
+    }
+    void rst_tmr1(void){
+    TMR0 = _tmr1_value;
+    PIR1bits.TMR1IF = 0;
+    }
+   
+//******************************************************************************
+//Funciones
+//******************************************************************************   
+
+    
